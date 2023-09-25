c21b15f2156208230e6857b2a8a3bc35beebd3cc
Cristian <cristianfierrosilva@gmail.com>
2023-09-25 11:15:02 -0500

    feat: add new family font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-      <Toaster rich-colors="" position="top-right"/>
+      <!-- <Toaster rich-colors="" position="top-right"/> -->
       <router-view />
     </div>
   </template>
@@ -10,3 +10,10 @@ export default {
     
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
+* {
+  font-family: Poppins, Helvetica, Arial, sans-serif;
+}
+</style>
