dc90c1f55b1939603ba3c985d41171d9bdc8bfb5
Cristian <cristianfierrosilva@gmail.com>
2023-09-25 11:11:49 -0500

    feat: add new common components

---
+++ src/components/common/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="flex w-full justify-center rounded-md transition ease-in-out duration-500 transform bg-CerberusLightBlue-default px-3 py-1.5 text-base font-medium leading-6 shadow-sm hover:bg-CerberusDarkBlue-default focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 pt-2 focus-visible:outline-indigo-600"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    primary: Boolean,
+    type: String,
+  },
+};
+</script>
---
+++ src/components/common/InputField.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <label
+      :for="name"
+      class="block text-base font-medium leading-6 text-gray-900"
+      >{{ label }}</label
+    >
+    <div class="mt-1">
+      <input
+        :id="name"
+        :name="name"
+        :type="type"
+        :placeholder="placeholder"
+        v-model="internalValue"
+        :required="required"
+        @input="$emit('update:modelValue', internalValue)"
+        class="w-full block py-2 rounded-md border p-4 placeholder:text-gray-400 focus:ring-CerberusLightBlue-default placeholder-gray-600 text-base sm:text-base sm:leading-6"
+        :readonly="readOnly"
+        :class="{ 'bg-gray-100 text-gray-500 cursor-not-allowed': readOnly }"
+        :maxlength="maxlength"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "label",
+    "name",
+    "type",
+    "value",
+    "required",
+    "placeholder",
+    "readOnly",
+    "maxlength",
+  ],
+  data() {
+    return {
+      internalValue: this.value,
+    };
+  },
+  watch: {
+    value(newValue) {
+      this.internalValue = newValue;
+    },
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
