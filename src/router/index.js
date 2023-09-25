b298f8eb669d0d80c83ae606cb412f8b9e4f0fb8
Cristian <cristianfierrosilva@gmail.com>
2023-09-25 11:14:09 -0500

    feat: add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,26 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/login",
-      name: "Login",
-      component: () => import("../views/LoginView.vue"),
+      path: "/auth",
+      name: "AuthView",
+      component: () => import("../layouts/AuthLayout.vue"),
+      children: [
+        {
+          path: "login",
+          name: "LoginView",
+          component: () => import("../views/auth/LoginView.vue"),
+        },
+        {
+          path:"set-password",
+          name:"SetPasswordView",
+          component: () => import("../views/auth/SetPasswordView.vue")
+        },
+        {
+          path: "forgot-password",
+          name: "ForgotPasswordView",
+          component: () => import("../views/auth/ForgotPasswordView.vue"),
+        }
+      ],
     },
   ],
 });
