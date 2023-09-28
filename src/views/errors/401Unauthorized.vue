e3e3533ba13e51a1c5d75e1927201c24a12cf85e
Daniel Quintero <quinterod164@gmail.com>
2023-09-28 08:13:02 -0500

    feat: :sparkles: update 401 view content

--- src/views/errors/401Unauthorized.vue
+++ src/views/errors/401Unauthorized.vue
@@ -1,13 +1,27 @@
 <template>
-    <h1>401 Unauthorized</h1>
-</template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    <main class="grid place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-screen">
+      <div class="text-center">
+        <p class="text-base font-semibold text-indigo-600">401</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Error 401: Unauthorized
+        </h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">
+            Lo sentimos, no tienes permiso para acceder a esta página.
+        </p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <router-link to="/login" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Volver
+          </router-link>
+        </div>
+      </div>
+    </main>
+    
+    </template>
+    
+    <script>
+    export default {};
+    </script>
+    
+    <style >
+    </style>
