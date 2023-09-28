28d4b33a4e5e8a99daf840a34ed44a820b0a365e
Cristian <cristianfierrosilva@gmail.com>
2023-09-28 01:31:32 -0500

    feat: add app use store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import router from "./router";
 import "./assets/styles/style.css";
 import App from "./App.vue";
+import store from "./store/index"
 
 const app = createApp(App);
 app.use(router);
+app.use(store)
 app.mount("#app");
