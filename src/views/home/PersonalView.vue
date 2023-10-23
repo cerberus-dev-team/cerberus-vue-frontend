5c30abcab2ed9310a630fbff9febb6d43bc1ea8f
Cristian <cristianfierrosilva@gmail.com>
2023-10-23 07:30:40 -0500

    feat: add Personalview

--- src/views/home/PersonalView.vue
+++ src/views/home/PersonalView.vue
@@ -1,13 +1,119 @@
-<template>
-  <div class="text-8xl">Personal View</div>
-</template>
+  <template>
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold leading-6 text-gray-900">
+            Personal Militar
+          </h1>
+          <p class="mt-2 text-sm text-gray-700">
+            Listado del personal militar registrado en el sistema
+          </p>
+        </div>
+      </div>
+      <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Nombre
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <template v-if="MilitaryUsers.length">
+                  <tr
+                    v-for="MilitaryUser in MilitaryUsers"
+                    :key="MilitaryUser.id"
+                  >
+                    <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                      <div class="flex items-center">
+                        <div class="h-11 w-11 flex-shrink-0">
+                          <img
+                            class="w-10 skeleton"
+                            :src="MilitaryUser.image_url"
+                            alt=""
+                          />
+                        </div>
+                        <div class="ml-4">
+                          <div class="font-medium text-gray-900 skeleton">
+                            {{ MilitaryUser.name}}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <div class="text-gray-900 skeleton">
+                        {{ MilitaryUser.name }}
+                      </div>
+                    </td>
+                    <td
+                      class="whitespace-nowrap px-3 py-5 text-sm text-gray-500 skeleton"
+                    >
+                      {{ MilitaryUser.name }}
+                    </td>
+                  </tr>
+                </template>
+                <!-- <template v-else>
+                  <tr v-for="index in 4" :key="index">
+                    <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                      <div class="flex items-center">
+                        <div class="h-11 w-11 flex-shrink-0">
+                          <div
+                            class="w-10 h-10 bg-gray-200 rounded-full animate-pulse"
+                          ></div>
+                        </div>
+                        <div class="ml-4">
+                          <div class="font-medium text-gray-900 animate-pulse">
+                            <div class="h-4 bg-gray-200 rounded w-1/2"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <div class="text-gray-900 animate-pulse">
+                        <div class="h-4 bg-gray-200 rounded w-1/2"></div>
+                      </div>
+                    </td>
+                    <td
+                      class="whitespace-nowrap px-3 py-5 text-sm text-gray-500 animate-pulse"
+                    >
+                      <div class="h-4 bg-gray-200 rounded w-1/4"></div>
+                    </td>
+                  </tr>
+                </template> -->
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
-<script>
-export default {
+  <script setup>
+  import { onMounted, computed } from "vue";
+  import { useStore } from "vuex";
 
-}
-</script>
+  const store = useStore();
 
-<style>
+  const MilitaryUsers = computed(() => store.state.militaryUsers);
 
-</style>
+  const getMilitaryUsers = () => store.dispatch("GET_BASIC_INFO");
+
+  onMounted(() => {
+    getMilitaryUsers();
+  });
+  </script>
