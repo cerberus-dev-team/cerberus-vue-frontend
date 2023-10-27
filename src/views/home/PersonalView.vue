51d30170aa4496926960cb2ddc44195d54a9858e
Cristian <cristianfierrosilva@gmail.com>
2023-10-26 20:29:10 -0500

    feat: add loader to personal view

--- src/views/home/PersonalView.vue
+++ src/views/home/PersonalView.vue
@@ -4,8 +4,8 @@
       <div class="sm:flex-auto">
         <h1 class="text-base font-semibold leading-6 text-gray-900">Personal</h1>
         <p class="mt-2 text-sm text-gray-700">
-          Una lista de todos los usuarios de su cuenta incluyendo su nombre, cargo,
-          correo electrónico.
+          Una lista de todos los usuarios de su cuenta incluyendo su nombre,
+          cargo, correo electrónico.
         </p>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
@@ -20,7 +20,66 @@
     <div class="mt-8 flow-root">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <table class="min-w-full divide-y divide-gray-300">
+          <!-- Esqueleto de carga -->
+          <div v-if="isLoading" class="animate-pulse">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Nombre
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Rango
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Status
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="n in 5" :key="n">
+                  <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                    <div class="flex items-center">
+                      <div class="h-11 w-11 flex-shrink-0">
+                        <div class="h-11 w-11 bg-gray-200 rounded-full"></div>
+                      </div>
+                      <div class="ml-4">
+                        <div class="font-medium text-gray-900 bg-gray-200 h-4 w-20"></div>
+                        <div class="mt-1 text-gray-500 bg-gray-200 h-4 w-32"></div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <div class="text-gray-900 bg-gray-200 h-4 w-16"></div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <span
+                      class="inline-flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-300"
+                    >Loading</span>
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+                  >
+                    <div class="h-4 w-12 bg-gray-200"></div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- Datos reales -->
+          <table v-else class="min-w-full divide-y divide-gray-300">
             <thead>
               <tr>
                 <th
@@ -41,17 +100,13 @@
                 >
                   Status
                 </th>
-
                 <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
                   <span class="sr-only">Edit</span>
                 </th>
               </tr>
             </thead>
-            <tbody class="divide-y divide-gray-200 bg-white">
-              <tr
-                v-for="MilitaryUser in MilitaryUsers"
-                :key="MilitaryUser.email"
-              >
+            <tbody>
+              <tr v-for="MilitaryUser in MilitaryUsers" :key="MilitaryUser.email">
                 <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                   <div class="flex items-center">
                     <div class="h-11 w-11 flex-shrink-0">
@@ -79,17 +134,17 @@
                 <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                   <span
                     class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-                    >Active</span
-                  >
+                  >Active</span>
                 </td>
                 <td
                   class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
                 >
-                  <a href="#" class="text-indigo-600 hover:text-CerberusDarkBlue-default"
-                    >Edit<span class="sr-only"
-                      >, {{ MilitaryUser.name }}</span
-                    ></a
+                  <a
+                    href="#"
+                    class="text-indigo-600 hover:text-CerberusDarkBlue-default"
                   >
+                    Edit<span class="sr-only">, {{ MilitaryUser.name }}</span>
+                  </a>
                 </td>
               </tr>
             </tbody>
@@ -101,16 +156,18 @@
 </template>
 
 <script setup>
-import { onMounted, computed } from "vue";
+import { onMounted, computed, ref } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
+const isLoading = ref(true)
 
 const MilitaryUsers = computed(() => store.state.militaryUsers);
 
 const getMilitaryUsers = () => store.dispatch("GET_ALL_USERS");
 
-onMounted(() => {
-  getMilitaryUsers();
+onMounted(async() => {
+ await getMilitaryUsers();
+ isLoading.value = false
 });
 </script>
